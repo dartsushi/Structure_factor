d1474d86f1c2e1bd72b7bed0bad7372ea99eafa0
dartsushi <aueda.phy@gmail.com>
2024-12-03 10:33:11 +0100

    test

---
+++ structure_factor.jl
@@ -0,0 +1,9 @@
+using MPSKit, TensorKit, KrylovKit
+T = ComplexF64
+#=
+    This is an implementation of the structure factor calculation based on PHYSICAL REVIEW B 105, 195140 (2022)
+=#
+
+χ = 8
+ψtop = InfiniteMPS(rand,T,ℂ^2,ℂ^χ)
+ψ_window = WindowMPS(rand,T,ℂ^2,ℂ^χ)
